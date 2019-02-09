ddf737d9c3e11386d565a620b86fa01a13b7030b
androclesleite <androclesleite@gmail.com>
2019-02-09 07:19:45 -0200

    Libary OK

--- test/index.php
+++ test/index.php
@@ -4,7 +4,7 @@ require __DIR__ . '/../lib_ext/autoload.php';
 
 use Notification\Email;
 
-$newEmail = new Email(2, "mail.wsmtec.com.br", "jefferson@wsmtec.com.br", "@Mudar010203", "tls", '587',
+$newEmail = new Email(2, "mail.wsmtec.com.br", "jefferson@wsmtec.com.br", "", "tls", '587',
     "jefferson@wsmtec.com.br", "Jefferson WSMTEC");
 $newEmail->sendEmail("Assunto de teste", "<p>Esse é o teste <b>teste</b></p>", "androclesleite@outlook.cm",
     "Jefferson Outlook", "androclesleite@gmail.com", "Jefferson Gmail");
