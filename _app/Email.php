637ca44a038573b3b1a14fe12ecaf1b3b4d93953
androclesleite <androclesleite@gmail.com>
2019-02-09 07:18:03 -0200

    Libary OK

--- _app/Email.php
+++ _app/Email.php
@@ -5,38 +5,36 @@ namespace Notification;
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-class Email {
+class Email
+{
 
     private $mail = \stdClass::class;
 
-    public function __construct() {
+    public function __construct($smtpDebug, $host, $user, $pass, $smtpSecure, $port, $setFromEmail, $setFromName)
+    {
 
 
         $this->mail = new PHPMailer(true);
-        $this->mail->SMTPDebug = 2;                                 // Enable verbose debug output
+        $this->mail->SMTPDebug = $smtpDebug;                        // Enable verbose debug output
         $this->mail->isSMTP();                                      // Set mailer to use SMTP
-        $this->mail->Host = 'mail.wsmtec.com.br';  // Specify main and backup SMTP servers
+        $this->mail->Host = $host;                                  // Specify main and backup SMTP servers
         $this->mail->SMTPAuth = true;                               // Enable SMTP authentication
-        $this->mail->Username = 'jefferson@wsmtec.com.br';                 // SMTP username
-        $this->mail->Password = '@Mudar010203';                           // SMTP password
-        $this->mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-        $this->mail->Port = 587;
+        $this->mail->Username = $user;                              // SMTP username
+        $this->mail->Password = $pass;                              // SMTP password
+        $this->mail->SMTPSecure = $smtpSecure;                      // Enable TLS encryption, `ssl` also accepted
+        $this->mail->Port = $port;
 
         $this->mail->CharSet = 'UTF-8';
         $this->mail->setLanguage('br');
         $this->mail->isHTML(true);
 
-        $this->mail->setFrom('androclesleite@gmail.com', 'Jefferson & CIA');
+        $this->mail->setFrom($setFromEmail, $setFromName);
 
-//        $this->mail->addAddress('joe@example.net', 'Joe User');     
-//        $this->mail->addAddress('ellen@example.com');             
-//        $this->mail->addReplyTo('info@example.com', 'Information');
-//        $this->mail->addCC('cc@example.com');
-//        $this->mail->addBCC('bcc@example.com');
     }
 
-    public function sendEmail($subject, $body, $replyEmail, $replyName, $addressEmail, $adrressName) {
-        $this->mail->Subject = (string) $subject;
+    public function sendEmail($subject, $body, $replyEmail, $replyName, $addressEmail, $adrressName)
+    {
+        $this->mail->Subject = (string)$subject;
         $this->mail->Body = $body;
 
         $this->mail->addReplyTo($replyEmail, $replyName);
--- test/index.php
+++ test/index.php
@@ -1,10 +1,11 @@
 <?php
 
-require __DIR__ . '/lib_ext/autoload.php';
+require __DIR__ . '/../lib_ext/autoload.php';
 
 use Notification\Email;
 
-$newEmail = new Email;
+$newEmail = new Email(2, "mail.wsmtec.com.br", "jefferson@wsmtec.com.br", "@Mudar010203", "tls", '587',
+    "jefferson@wsmtec.com.br", "Jefferson WSMTEC");
 $newEmail->sendEmail("Assunto de teste", "<p>Esse é o teste <b>teste</b></p>", "androclesleite@outlook.cm",
     "Jefferson Outlook", "androclesleite@gmail.com", "Jefferson Gmail");
 
